4840fb1d29aa7c85605e2a08ae297650352312a9
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-04-24 13:22:11 +0200

    Create the header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,22 @@
-import './globals.css'
+import "./globals.css";
+import Header from "@/components/header";
 
 export const metadata = {
-  title: 'IMDB Clone',
-  description: 'This is the IMDB clone website',
-}
+  title: "IMDB Clone",
+  description: "This is the IMDB clone website",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/*Header*/}
+        <Header />
+
+        {/*Navbar*/}
+        {/*SearchBox*/}
+        {children}
+      </body>
     </html>
-  )
+  );
 }
