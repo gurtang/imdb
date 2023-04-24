4aa17dd404781ebc8828d48b7995f973f47ed893
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-04-24 17:29:51 +0200

    Created About page

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className={"max-w-6xl mx-auto space-y-4"}>
+      <h1 className={"text-2xl font-medium text-amber-600"}>About</h1>
+      <p>
+        This is a copy of IMDb site, with movies. And iformation about movies.
+      </p>
+      <p>This is writen in next js</p>
+    </div>
+  );
+}
+
+export default About;
